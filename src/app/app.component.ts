c8e9b904bb2543f527791f66d4c2c825e71cb430
Nick Hart <nhartmusic@gmail.com>
2021-03-18 18:09:36 +0100

    testing out some templates and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directory';
+  title = 'turn the fuck up bruh';
+  name = "NH"
 }
